a913a4cc3e8b8a03fbb612f535bbeb182830c6a6
Johnnatan <contatojohnnatan@gmail.com>
2021-10-20 17:45:50 -0300

    feat: creating dashboard header

--- src/components/Dashboard/DashboardHeader/index.jsx
+++ src/components/Dashboard/DashboardHeader/index.jsx
@@ -1,6 +1,26 @@
+import { Header } from "./style"
+import logoHeader from '../../../assets/images/Header/logoHeader.png'
+import avatar from '../../../assets/images/Header/avatar.png'
+import { Avatar } from "@material-ui/core"
+
 const DashboardHeader = () => {
 	return (
-		<h1>header</h1>
+		<Header>
+			<div className='container'>
+				<a href="#">
+					<img alt='Guia Vet' src={logoHeader} />
+				</a>
+				<div className="user-profile">
+					<Avatar
+						alt="Ciro Bottini"
+						src={avatar}
+					/>
+					<span className="username">
+						Ciro Bottini
+					</span>
+				</div>
+			</div>
+		</Header>
 	)
 }
 
---
+++ src/components/Dashboard/DashboardHeader/style.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import theme from "../../../utils/theme";
+
+export const Header = styled.div`
+background-color: ${theme.header};
+min-height: 65px;
+width: 100%;
+display: flex;
+flex-flow: column nowrap;
+align-items: center;
+justify-content: center;
+
+.container {
+	max-width: 1100px;
+	width: 80%;
+	margin: 0 auto;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	flex-flow: row nowrap;
+
+	.user-profile {
+		display: flex;
+		flex-flow: row nowrap;
+		align-items: center;
+		justify-content: center;
+
+		.username {
+			margin-left: 10px;
+			font-weight: 500;
+			color: ${theme.fontColorLight}
+		}
+	}
+}
+`
